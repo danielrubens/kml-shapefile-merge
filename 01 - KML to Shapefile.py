a327c7bcd8ce5d457779971a851d4cbc03f0e338
danielrubens <51061212+danielrubens@users.noreply.github.com>
2021-01-18 16:30:19 -0300

    Update 01 - KML to Shapefile.py

--- 01 - KML to Shapefile.py
+++ 01 - KML to Shapefile.py
@@ -9,7 +9,7 @@ projeto = QgsProject.instance()
 projetoPath = os.path.dirname(projeto.fileName())
 crs = QgsCoordinateReferenceSystem("EPSG:4674")
 
-caminhos = [os.path.join('C://Users//Daniel//Desktop//kml-shapefile', nome) for nome in os.listdir('C://Users//Daniel//Desktop//kml-shapefile')]
+caminhos = [os.path.join('C://Users//Desktop//kml-shapefile', nome) for nome in os.listdir('C://Users//Desktop//kml-shapefile')]
 arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
 kmls = [arq for arq in arquivos if arq.lower().endswith(".kml")]
 				
@@ -24,5 +24,5 @@ for nome in kmls:
 	displayName =  nome[0:len(nome) -4].split('\\')[len(nome[0:len(nome) -4].split('\\')) - 1]
 	filename = QgsVectorLayer(fileroute,displayName,"ogr")
 	#QgsMapLayerRegistry.instance().addMapLayer(filename,False)
-	limite = "C://Users//Daniel//Desktop//kml-shapefile//" + displayName +".shp"
+	limite = "C://Users//Desktop//kml-shapefile//" + displayName +".shp"
 	iface.addVectorLayer(limite, " " + displayName, "ogr")
