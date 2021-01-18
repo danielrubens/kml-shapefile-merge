09843bf56724e899becbe4b69524bfb1c17f0d7c
danielrubens <51061212+danielrubens@users.noreply.github.com>
2021-01-18 16:37:43 -0300

    Update 03 - COMPLETE - Convert KML's to shapefile and merge into a single.py

--- 03 - COMPLETE - Convert KML's to shapefile and merge into a single.py
+++ 03 - COMPLETE - Convert KML's to shapefile and merge into a single.py
@@ -9,7 +9,7 @@ projeto = QgsProject.instance()
 projetoPath = os.path.dirname(projeto.fileName())
 crs = QgsCoordinateReferenceSystem("EPSG:4674")
 
-caminhos = [os.path.join('C://Users//Daniel//Desktop//kml-shapefile', nome) for nome in os.listdir('C://Users//Daniel//Desktop//kml-shapefile')]
+caminhos = [os.path.join('C://Users//Desktop//kml-shapefile', nome) for nome in os.listdir('C://Users//Desktop//kml-shapefile')]
 arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
 kmls = [arq for arq in arquivos if arq.lower().endswith(".kml")]
 				
@@ -17,19 +17,18 @@ fileroute = ''
 fKML = []
 
 for nome in kmls:
-
 			
 	vlayer = QgsVectorLayer(nome, "line", "ogr")
 	writer = QgsVectorFileWriter.writeAsVectorFormat(vlayer, nome[0:len(nome) -3] + 'shp', "utf-8", crs, "ESRI Shapefile")
 	displayName =  nome[0:len(nome) -4].split('\\')[len(nome[0:len(nome) -4].split('\\')) - 1]
 	filename = QgsVectorLayer(fileroute,displayName,"ogr")
 	#QgsMapLayerRegistry.instance().addMapLayer(filename,False)
-	limite = "C://Users//Daniel//Desktop//kml-shapefile//" + displayName +".shp"
+	limite = "C://Users//Desktop//kml-shapefile//" + displayName +".shp"
 	iface.addVectorLayer(limite, " " + displayName, "ogr")
 
 
-outputMergefn = 'C://Users//Daniel//Desktop//kml-shapefile//rodovias.shp'
-directory = "C://Users//Daniel//Desktop//kml-shapefile//"
+outputMergefn = 'C://Users//Desktop//kml-shapefile//rodovias.shp'
+directory = "C://Users//Desktop//kml-shapefile//"
 fileStartsWith = 'rod'
 fileEndsWith = '.shp'
 driverName = 'ESRI Shapefile'
@@ -40,9 +39,6 @@ out_ds = out_driver.CreateDataSource(outputMergefn)
 out_layer = out_ds.CreateLayer(outputMergefn, geom_type=geometryType)
 
 fileList = os.listdir(directory)
-
-
-
 
 for file in fileList:
     if file.startswith(fileStartsWith) and file.endswith(fileEndsWith):
@@ -56,4 +52,4 @@ for file in fileList:
             out_feat = None
             out_layer.SyncToDisk()
 
-iface.addVectorLayer('C://Users//Daniel//Desktop//kml-shapefile//rodovias.shp', 'rodovias','ogr')
+iface.addVectorLayer('C://Users//Desktop//kml-shapefile//rodovias.shp', 'rodovias','ogr')
